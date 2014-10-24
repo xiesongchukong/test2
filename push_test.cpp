0c8be35f5469779adbf89188c2fdcd35b729c138
chuanweizhang <chuanweizhang2013@gmail.com>
2014-10-24 10:42:23 +0800

    content push

---
+++ push_test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void test_pushcontent(){
+    //this function used for test git content push 
+}
+int main(int argc,char ** argv ){
+    test_pushcontent();
+    printf("....");
+}
